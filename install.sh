c73f5ee598abdcba349770c2c0ff9b3c27eb072c
david.shrader <david.shrader@spottradingllc.com>
2017-08-31 10:38:58 +0530

    Added ssh agent

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # create symlinks
 ln -fs ~/dot-files/.bashrc ~/.bashrc
 ln -fs ~/dot-files/.bash_profile ~/.bash_profile
+ln -fs ~/dot-files/.bash_logout ~/.bash_logout
 ln -fs ~/dot-files/.config ~/.config
 ln -fs ~/dot-files/.gitconfig ~/.gitconfig
 ln -fs ~/dot-files/.gitignore_global ~/.gitignore_global 
@@ -14,7 +15,7 @@ ln -fs ~/dot-files/.vimrc ~/.vimrc
 # set global git parameters
 git config --global core.excludesfile '~/.gitignore_global' 
 
-# switch to the `~/.dot-files` directory, and fetch submodules:
+# fetch submodules:
 git submodule init && git submodule update
 
 # install tmux plugins (can be automated)
